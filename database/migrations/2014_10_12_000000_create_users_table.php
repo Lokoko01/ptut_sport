bf1228a3e915b9e2a9d70cdc80f57b21353638ed
sloanfardele <sloan.fardele@outlook.fr>
2017-03-06 10:24:03 +0100

    Add string size for database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('lastname');
             $table->string('firstname');
             $table->string('dateOfBirth');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
