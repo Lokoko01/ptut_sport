ad95cd8d42af058a60c529cd88932b953aa632b7
sloanfardele <sloan.fardele@outlook.fr>
2017-03-09 16:52:02 +0100

    On fait des jolis radio button

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,37 +7,31 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">S'inscrire</div>
                     <div class="panel-body">
+                    <?php
+                    $columnSizes = [
+                        'md' => [4, 6]
+                    ];
+                    ?>
 
-                            <?php
-                                $columnSizes = [
-                                    'md' => [4, 6]
-                                ];
-                            ?>
+                    <!-- Pour faire un form bien on peut utiliser celui la avec le $columnSizes d'enhaut-->
+                        {{--  {!! BootForm::openHorizontal($columnSizes)->action(route('register')) !!}--}}
 
-                            {!! BootForm::openHorizontal($columnSizes)->action(route('register')) !!}
-                            {!! BootForm::text('Nom', 'lastname') !!}
-                            {!! BootForm::text('Prénom', 'firstname') !!}
-                            {!! BootForm::date('Date de naissance', 'dateOfBirth') !!}
-                            <div class="form-group">
-                                {!! Form::label('sex', 'Sexe', array('class' => 'col-md-4 control-label')) !!}
-                                <div class="col-md-6">
-                                    {{ Form::radio('sex', 'male', true) }}  {!! Form::label('male', 'Homme') !!}<br>
-                                    {{ Form::radio('sex', 'female') }}  {!! Form::label('female', 'Femme') !!}
-                                </div>
-                            </div>
-                            {!! BootForm::email('Email', 'email') !!}
-                            {!! BootForm::select('Unité de Formation de Recherche', 'ufr')->options($ufrs) !!}
-                            {!! BootForm::password('Mot de passe', 'password') !!}
-                            <div class="form-group">
-                                <label for="password-confirm" class="col-md-4 control-label">Confirmation de mot de passe</label>
+                        {!! BootForm::open()->action(route('register')) !!}
 
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                           name="password_confirmation" required>
-                                </div>
-                            </div>
-                            {!! BootForm::submit("S'inscrire")->class('btn btn-primary') !!}
-                            {!! BootForm::close() !!}
+                        {!! BootForm::text('Nom', 'lastname') !!}
+                        {!! BootForm::text('Prénom', 'firstname') !!}
+                        {!! BootForm::date('Date de naissance', 'dateOfBirth') !!}
+
+                        {!! BootForm::inlineRadio('Homme', 'sex', 'male') !!}
+                        {!! BootForm::inlineRadio('Femme', 'sex', 'female') !!}
+
+                        {!! BootForm::email('Email', 'email') !!}
+                        {!! BootForm::select('Unité de Formation de Recherche', 'ufr')->options($ufrs) !!}
+                        {!! BootForm::password('Mot de passe', 'password') !!}
+                        {!! BootForm::password('Confirmation du mot de passe', 'password_confirmation') !!}
+                        {!! BootForm::submit("S'inscrire")->class('btn btn-primary') !!}
+
+                        {!! BootForm::close() !!}
                     </div>
                 </div>
             </div>
