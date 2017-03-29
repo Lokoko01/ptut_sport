9e0fbf815b3fadd2422f1644dc5dab3909f1a3c4
Lokoko01 <martin.rascle@gmail.com>
2017-03-29 18:07:21 +0200

    Modified test mail

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -3,9 +3,9 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
+                    <div class="panel-heading">Info</div>
 
-                    <p>Veuillez suivre ce lien pour accéder au formulaire d'inscription :</p>
-                    <a href="http://www.google.com"></a>
+                    <h2>Prise de contact</h2>
 
                 </div>
             </div>
