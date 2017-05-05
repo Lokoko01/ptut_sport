d0ed4b9e2f6aa1d96f8f593884ce9055ad775058
Lokoko01 <martin.rascle@gmail.com>
2017-05-05 16:11:23 +0200

    search bar

--- database/migrations/2017_05_05_132008_create_studentWishes_table.php
+++ database/migrations/2017_05_05_132008_create_studentWishes_table.php
@@ -19,6 +19,7 @@ class CreateStudentWishesTable extends Migration
             $table->integer('session_id')->unsigned();
             $table->foreign('session_id')->references('id')->on('sessions');
             $table->integer('rank');
+            $table->integer('sportNumber');
             $table->boolean('spareTime');
             $table->timestamps();
         });
