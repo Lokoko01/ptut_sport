09fa7c93966db21f6931eb7e417a8b1d9efe02fd
Lokoko01 <martin.rascle@gmail.com>
2017-06-06 14:42:51 +0200

    Modify table marks

--- database/migrations/temp/2017_03_08_000012_create_marks_table.php
+++ database/migrations/temp/2017_03_08_000012_create_marks_table.php
@@ -15,10 +15,8 @@ class CreateMarksTable extends Migration
     {
         Schema::create('marks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('student_id')->unsigned();
-            $table->foreign('student_id')->references('id')->on('students');
-            $table->integer('session_id')->unsigned();
-            $table->foreign('session_id')->references('id')->on('sessions');
+            $table->integer('student_sport_id')->unsigned();
+            $table->foreign('student_sport_id')->references('id')->on('student_sport');
             $table->integer('mark');
             $table->string('comment');
             $table->timestamps();
