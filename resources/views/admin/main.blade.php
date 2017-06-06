8a11853362379c077e7cf352d25699388fe11cc1
sloanfardele <sloan.fardele@outlook.fr>
2017-06-06 11:46:52 +0200

    litle route fix

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -8,16 +8,16 @@
                     <div class="panel-heading">Accueil - Professeur</div>
                     <div class="panel-body panel-admin panel-home">
                         <div class="button-admin flex-div">
-                            <a href="/admin/addSession" class="btn btn-default">Ajouter une séance</a><br>
-                            <a href="/admin/addProfessor" class="btn btn-default">Ajouter un professeur</a><br>
-                            <a href="/admin/addAdmin" class="btn btn-default">Ajouter un admin</a><br>
-                            <a href="/admin/addAdmin" class="btn btn-default">Ajouter un sport</a><br>
+                            <a href="{{route('add_session')}}" class="btn btn-default">Ajouter une séance</a><br>
+                            <a href="{{route('add_professor')}}" class="btn btn-default">Ajouter un professeur</a><br>
+                            <a href="{{route('add_admin')}}" class="btn btn-default">Ajouter un admin</a><br>
+                            <a href="{{route('sport')}}" class="btn btn-default">Ajouter un sport</a><br>
 
                             <a href="{{ route('home') }}" class="btn btn-default">Editer une note</a><br>
                             <a href="{{ route('home') }}" class="btn btn-default">Désinscrire un étudiant</a><br>
                             <a href="{{ route('home') }}" class="btn btn-default">Ajouter un étudiant à un sport</a><br>
                             <a href="{{ route('home') }}" class="btn btn-default">Générer un fichier Excel des notes</a><br>
-                            <a href="/admin/listStudents" class="btn btn-default">Afficher la liste des étudiants</a><br>
+                            <a href="{{ route('students')}}" class="btn btn-default">Afficher la liste des étudiants</a><br>
                             <a href="{{ route('home') }}" class="btn btn-default">RAZ le semestre</a><br>
                         </div>
                         <div class="informations-messages flex-div">
