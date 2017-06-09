8db20cd166c49bfddae24e4325fa8bf605443493
sloanfardele <sloan.fardele@outlook.fr>
2017-06-09 16:00:53 +0200

    fix

--- resources/views/professor/main.blade.php
+++ resources/views/professor/main.blade.php
@@ -16,8 +16,8 @@
                             <a href="{{ route('checkAbsences') }}" class="btn btn-default">Faire l'appel</a><br>
                             <a href="{{ route('students_by_sport_excel_prof')}}" class="btn btn-default">
                                 Générer les feuilles d'appel <i class="fa fa-file-excel-o"></i></a><br>
-                            <a href="{{ route('students_by_sport_pdf_prof')}}" class="btn btn-default">
-                                Générer les feuilles d'appel <i class="fa fa-file-pdf-o"></i></a><br>
+                            {{--<a href="{{ route('students_by_sport_pdf_prof')}}" class="btn btn-default">--}}
+                                {{--Générer les feuilles d'appel <i class="fa fa-file-pdf-o"></i></a><br>--}}
                             <a href="{{ route('assignMark') }}" class="btn btn-default">Rentrer les notes des
                                 étudiants</a><br>
                             <a href="{{ route('professor_message') }}" class="btn btn-default">Envoyer un message</a><br>
